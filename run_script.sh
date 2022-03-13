564233969e013f0a9abf9d8c39fbd92f64a0ca41
Anton Patsev <patsev.anton@gmail.com>
2022-03-13 10:47:44 +0600

    Remove -vvv

--- run_script.sh
+++ run_script.sh
@@ -4,7 +4,7 @@ set -e
 
 export GITLAB_ROOT_PASSWORD="source_me_from_secret_manager"
 
-ansible-playbook -c local -i localhost -vvv -i inventory.yml -e GITLAB_ROOT_PASSWORD='{{ lookup("env", "GITLAB_ROOT_PASSWORD") }}', playbook.yml
+ansible-playbook -c local -i localhost -i inventory.yml -e GITLAB_ROOT_PASSWORD='{{ lookup("env", "GITLAB_ROOT_PASSWORD") }}', playbook.yml
 
 echo "Variable GITLAB_ROOT_PASSWORD is:"
 echo "$GITLAB_ROOT_PASSWORD"
