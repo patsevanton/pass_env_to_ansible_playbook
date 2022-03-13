4b14656822e45a7b0a54db57c4710118cc6a144d
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2022-03-13 10:42:51 +0600

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -4,7 +4,7 @@ set -e
 
 export GITLAB_ROOT_PASSWORD="source_me_from_secret_manager"
 
-ansible-playbook -vvv -i inventory.yml playbook.yml -e GITLAB_ROOT_PASSWORD='{{ lookup("env", "GITLAB_ROOT_PASSWORD") }}'
+ansible-playbook -c local -i localhost -vvv -i inventory.yml -e GITLAB_ROOT_PASSWORD='{{ lookup("env", "GITLAB_ROOT_PASSWORD") }}', playbook.yml
 
 echo "Variable GITLAB_ROOT_PASSWORD is:"
 echo "$GITLAB_ROOT_PASSWORD"
